5c99fae2ddbd51b3db7f8af848c0243d6f81ca11
Florian Popa <florian@webgenerals.com>
2016-06-21 16:24:45 +0300

    angular-workshop-1: Add data binding sample.

---
+++ angular-1/workshop-1/data-binding/js/DataBinding1.js
@@ -0,0 +1,30 @@
+/**
+ * DataBinding1 created on 6/21/16 4:08 PM.
+ *
+ * @description [To be completed]
+ * @author Florian Popa <florian@webgenerals.com>
+ */
+(function () {
+    "use strict"; // jshint ignore:line
+
+    angular
+        .module('DataBinding1', []);
+
+    angular
+        .module('DataBinding1')
+        .controller('DataBinding1Controller', ['$scope', function ($scope) {
+
+            $scope.myModel = {
+                name: 'Data binding',
+                type: true
+            };
+
+            console.log($scope.myModel);
+
+            $scope.saveChanges = function () {
+                console.log($scope.myModel);
+            };
+            
+        }]);
+
+})();
---
+++ angular-1/workshop-1/data-binding/readme.md
@@ -0,0 +1,4 @@
+> Data binding sample
+
+- Open the console and observe the log. This is how the model is initialized.
+- Change the values in the fields and click save. Observe the changes in the console.
